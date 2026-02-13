3c364990ef880b0cfee01d0c3197042c75dfefad
Snowy <Showhanur@hotmail.com>
2026-02-14 00:05:02 +0400

    fix(prisma): Replace SQLite adapter with PostgreSQL adapter

--- src/_core/lib/prisma.ts
+++ src/_core/lib/prisma.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const connectionString = process.env.DATABASE_URL ?? 'file:./prisma/bot.db';
 
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const adapter = new PrismaPg({ url: connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma }
